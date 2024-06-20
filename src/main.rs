0d580377a21f79f5989ac4708196a2f8e4de19f9
Alex K <kleydints@petalmail.com>
2024-06-20 11:41:45 +0300

    added display i2c ssd1306

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,17 @@
 
 use core::cell::RefCell;
 use critical_section::Mutex;
+use embedded_graphics::{
+    geometry::Dimensions,
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::Point,
+    text::{Alignment, Text},
+    Drawable,
+};
+use ssd1306::{
+    prelude::*, rotation::DisplayRotation, size::DisplaySize128x64, I2CDisplayInterface, Ssd1306,
+};
 
 use esp_backtrace as _;
 use esp_hal::{
@@ -10,6 +21,7 @@ use esp_hal::{
     delay::Delay,
     // the self enables the use of gpio
     gpio::{self, Event, Input, Io, Level, Output, Pull},
+    i2c::I2C,
     macros::ram,
     peripherals::Peripherals,
     prelude::*,
@@ -43,7 +55,34 @@ fn main() -> ! {
     let led = Output::new(io.pins.gpio2, Level::Low);
     critical_section::with(|cs| LED.borrow_ref_mut(cs).replace(led));
 
-    // set up an i2c
+    // Connect the ssd1306 display
+    let sda = io.pins.gpio4;
+    let scl = io.pins.gpio5;
+
+    // set up an i2c connection for the display
+    let i2c_display = I2C::new(peripherals.I2C0, sda, scl, 100.kHz(), &clocks, None);
+
+    let interface = I2CDisplayInterface::new(i2c_display);
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    // Specify text style
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+    Text::with_alignment(
+        "INTERRUPT EXPERIMENT",
+        display.bounding_box().center() + Point::new(0, 14),
+        text_style,
+        Alignment::Center,
+    )
+    .draw(&mut display)
+    .unwrap();
+
+    display.flush().unwrap();
 
     loop {
         // println!("Privet Mir!");
